b5518922f72fd9430d541923346c0e97d8e3d538
BookFlower <1753980101@qq.com>
2022-10-03 15:46:27 +0800

    add language change

---
+++ index.js
@@ -0,0 +1,8 @@
+function show() {
+    var show_part = document.querySelector('.part');
+    show_part.style.display = 'block';
+}
+function hide() {
+    var show_part = document.querySelector('.part');
+    show_part.style.display = 'none';
+}
